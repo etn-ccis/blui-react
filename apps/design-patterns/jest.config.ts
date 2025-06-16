4b7d2494baf9edc488cfa6382396ca49606a91d2
Suraj Karambe <surajakarambe@eaton.com>
2025-06-16 16:31:47 +0530

    Ran test cases

--- apps/design-patterns/jest.config.ts
+++ apps/design-patterns/jest.config.ts
@@ -5,8 +5,17 @@ export default {
         '^.+\\.tsx?$': 'ts-jest',
         // process `*.tsx` files with `ts-jest`
     },
+    transformIgnorePatterns: [
+        '/node_modules/(?!@mui|@brightlayer-ui)',
+    ],
     moduleNameMapper: {
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     },
     moduleDirectories: ['node_modules', '<rootDir>/../'],
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
 };
