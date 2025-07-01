7bccf4b4b60dc35ecee7f174c547fd8aa1e53482
ektaghag-eaton <ektanghag@eaton.com>
2025-07-01 10:12:11 +0530

    add concurrently package

--- apps/showcase/vite.config.ts
+++ apps/showcase/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +13,19 @@ export default defineConfig({
         open: true,
         // this sets a default port to 3000
         port: 3000,
+        fs: {
+            allow: [
+                '..', // allows access to the parent directory
+                '../packages', // allows access to the packages directory
+            ],
+        },
     },
     optimizeDeps: {
         exclude: ['@brightlayer-ui/react-themes', '@brightlayer-ui/react-components'],
+    },
+    resolve: {
+        alias: {
+            '@brightlayer-ui/react-components': path.resolve(__dirname, '../../packages/react-components/src/*'),
+        },
     },
 });
