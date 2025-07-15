c177ab1e6635537c5dfb20fbef3994b7f5250b57
Suraj Karambe <surajakarambe@eaton.com>
2025-07-15 18:04:13 +0530

    Update module name mapper to support jpg

--- apps/design-patterns/jest.config.ts
+++ apps/design-patterns/jest.config.ts
@@ -7,7 +7,7 @@ export default {
     },
     transformIgnorePatterns: ['/node_modules/(?!@mui|@brightlayer-ui)'],
     moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/test/__mocks__/fileMock.js',
         "\\.(css|less)$": "<rootDir>/test/__mocks__/fileMock.js",
         "^@brightlayer-ui/react-components$": "<rootDir>/../../packages/component-library/src/index.ts",
         "^@brightlayer-ui/react-themes$": "<rootDir>/../../packages/themes/src/index.ts"
