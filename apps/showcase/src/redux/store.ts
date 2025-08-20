347fc4a69f5bb4a11a2629651cf1a4a2e1581f41
Komal Dhere <KomalGDhere@eaton.com>
2025-08-20 12:35:23 +0530

    updated package.json for showcase

--- apps/showcase/src/redux/store.ts
+++ apps/showcase/src/redux/store.ts
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducers';
 
-export const store = createStore(rootReducer(), applyMiddleware(thunk));
+export const store = configureStore({
+    reducer: rootReducer(),
+    // Redux Toolkit includes thunk middleware by default
+});
