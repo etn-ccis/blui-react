bd4d35116bee358fc8c37ff6b1601da317a12944
manojleaton <manojlokesh@eaton.com>
2026-05-25 12:24:52 +0530

    paper shadow updates

---
+++ packages/themes/src/componentStylesOverrides/MuiPaper.ts
@@ -0,0 +1,9 @@
+import { Components, Theme, CssVarsTheme } from '@mui/material/styles';
+
+export default {
+    styleOverrides: {
+        root: ({ theme }) => ({
+            boxShadow: theme.vars.palette.shadows.level1,
+        }),
+    },
+} as Components<Omit<Theme, 'components' | 'palette'> & CssVarsTheme>['MuiPaper'];
