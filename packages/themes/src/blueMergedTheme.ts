b17241c1c838e801a75bcbe6bf52fadfc052d31c
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-05-27 18:41:05 +0530

    Fixed theme color override issues issues like

--- packages/themes/src/blueMergedTheme.ts
+++ packages/themes/src/blueMergedTheme.ts
@@ -46,6 +46,7 @@ import MuiFormHelperText from './componentStylesOverrides/MuiFormHelperText';
 import MuiToggleButtonGroup from './componentStylesOverrides/MuiToggleButtonGroup';
 import MuiToggleButton from './componentStylesOverrides/MuiToggleButton';
 import MuiIconButton from './componentStylesOverrides/MuiIconButton';
+import MuiPaper from './componentStylesOverrides/MuiPaper';
 import MuiRadio from './componentStylesOverrides/MuiRadio';
 import MuiFormControlLabel from './componentStylesOverrides/MuiFormControlLabel';
 
@@ -174,6 +175,7 @@ export const blueThemes = createTheme({
         MuiToggleButtonGroup: MuiToggleButtonGroup,
         MuiToggleButton: MuiToggleButton,
         MuiIconButton: MuiIconButton,
+        MuiPaper: MuiPaper,
         MuiRadio: MuiRadio,
         MuiFormControlLabel: MuiFormControlLabel,
     },
--- packages/themes/src/componentStylesOverrides/MuiButtonGroup.ts
+++ packages/themes/src/componentStylesOverrides/MuiButtonGroup.ts
@@ -9,8 +9,8 @@ export default {
                 border: `1px solid ${Color(BLUIColors.black[500]).alpha(0.12).string()}`,
             },
         }),
-        outlined: () => ({
-            color: BLUIColors.black[50],
+        outlined: ({ theme }) => ({
+            color: theme.vars.palette.text.primary,
         }),
         groupedText: ({ theme }) => ({
             ...theme.applyStyles('dark', {
@@ -24,6 +24,11 @@ export default {
                 '&:hover': {
                     borderRightColor: 'transparent',
                 },
+            },
+        }),
+        groupedContainedHorizontal: ({ theme }) => ({
+            '&:not(:last-of-type)': {
+                borderRightColor: theme.vars.palette.divider,
             },
         }),
         groupedTextPrimary: ({ theme }) => ({
