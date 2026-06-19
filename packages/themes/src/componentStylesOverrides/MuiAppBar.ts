8e09977750bbc431ef43a6dd19295b840204e1e9
manojleaton <manojlokesh@eaton.com>
2026-06-19 13:22:26 +0530

    removed boxshadow

--- packages/themes/src/componentStylesOverrides/MuiAppBar.ts
+++ packages/themes/src/componentStylesOverrides/MuiAppBar.ts
@@ -4,7 +4,6 @@ import * as BLUIColors from '@brightlayer-ui/colors';
 export default {
     styleOverrides: {
         root: ({ theme }) => ({
-            boxShadow: theme.vars.palette.shadows.level2,
             ...theme.applyStyles('dark', {
                 backgroundImage: 'none',
             }),
